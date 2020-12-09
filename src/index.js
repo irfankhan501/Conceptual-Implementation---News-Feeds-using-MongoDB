9b282abee6afa19dc268f5c51dddde24d60b4cc5
Irfan Khan <irfankhan501@outlook.com>
2020-12-09 09:51:09 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const { newsArticleModel } = require("./connector");
+// const mongoose = require("mongoose");
+const app = express();
+const port = 8080;
 
-const onePageArticleCount = 10
-
+const onePageArticleCount = 10;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get("/newFeeds", async (req, res) => {
+  const { limit, offset } = req.query;
+  if (isNaN(limit) || isNaN(offset)) {
+    const result = await newsArticleModel.find().skip(0).limit(10);
+    res.send(result);
+  } else {
+    const result = await newsArticleModel
+      .find()
+      .skip(Number(offset))
+      .limit(Number(limit));
+    res.send(result);
+  }
+});
 
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
